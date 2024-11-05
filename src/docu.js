7a1b6840c7431cdf1c46098da13fb20cbe2d3a7a
Edu-Reyna <eduardor1901g@gmail.com>
2024-11-05 17:13:23 -0500

    cambios color login

---
+++ src/docu.js
@@ -0,0 +1,7 @@
+function showContent(section) {
+    var sections = document.getElementsByClassName('section');
+    for (var i = 0; i < sections.length; i++) {
+        sections[i].style.display = 'none';
+    }
+    document.getElementById(section).style.display = 'block';
+}
